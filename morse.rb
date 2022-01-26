c7232d11c51f34962a2d4d5976aef48921376999
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-26 22:54:34 +0200

    Add changes

--- morse.rb
+++ morse.rb
@@ -1,14 +1,23 @@
-morse = {'A' => '.-', 'B' => -'-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.' , 'G' => ' --.',
+$morse = {'A' => '.-', 'B' => -'-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.' , 'G' => ' --.',
   'H' => ' ....', 'I' => '..', 'J' => '.---' , 'K' => '-.-','L' => '.-..' , 'M' => '--', 'N' => '-.','O' => '---' , 'P' => '.--.','Q' => '--.-','R' => '.-.', 
-'S' => '...' ,'T' => '-' ,'U' => '..-', 'V' => '...-', 'W' => '.--'; 'X' => '-..-', 'Y' => '-.--' ,'Z' => '--..'}
+'S' => '...' ,'T' => '-' ,'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-', 'Y' => '-.--' ,'Z' => '--..'}
 
 
-def decode_morse (arg)
-    $morse.each do |key, value|
-      if arg == value
-        print key
-      end
-    end
-end
+# def decode_morse (arg)
+#   decoded = arg.split(' ')
+#   decoded.each {|word| if word }
+  
+#     $morse.each do |key, value|
+#       if arg == value
+#         print key
+#       end
+#     end
+# end
 
-decode_morse '.'
+# decode_morse "-- -.--   -. .- -- ."
+
+# def decode_morse(arg)
+#   word = arg.split
+#   result = ''
+#   word.each do |char|
+#     result += de
