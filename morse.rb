3c1d58c63789e6d2cda2edcc8618b1be514efa74
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-27 13:11:14 +0200

    Add method for single word

--- morse.rb
+++ morse.rb
@@ -1,4 +1,4 @@
-def decoded_char(char)
+
   @morse_to_char = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
     '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
@@ -6,29 +6,37 @@ def decoded_char(char)
     '-.--' => 'Y', '--..' => 'Z', '.----' => '1', '..---' => '2', '...--' => '3', '....-' => '4', '.....' => '5',
     '-....' => '6', '--...' => '7', '---..' => '8', '----.' => '9', '-----' => '0'
   }
-  @result = @morse_to_char[char]
-  if @result.nil?
-    ' '
-  else
-    @result
-  end
-end
+ 
 
-def decoded_word(word)
-  word = word.split
-  result = ''
-  word.each do |char|
-    result += decoded_char(char)
-  end
-  result
-end
+def decoded_character(word)
+ @morse_to_char[word]
+end 
 
-def decode(message)
-  message = message.split('   ')
-  result = []
-  message.each do |char|
-    result.push(decoded_word(char))
-  end
-  result.join(' ')
+def decode_word(message)
+  chars = message.split
+  char = chars.map
+ 
 end
+# def decode_char(morse_char)
+#   @morse_alphabet[morse_char]
+# end
+
+# def decode_word(morse_word)
+#   chars = morse_word.split
+#   char = chars.map do |c|
+#     decode_char(c)
+#   end
+#   char.join
+# end
+
+# def decode(morse_phrase)
+#   words = morse_phrase.split('   ')
+#   words = words.map do |w|
+#     decode_word(w)
+#   end
+#   words.join(' ')
+# end
+
+puts decode(".-")
+puts decode("-- -.--")
 puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
